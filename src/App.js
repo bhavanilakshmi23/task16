501cc52782d8f7c72b947b4de1e4c43e53f05dbd
bhavani <127599832+bhavanilakshmi23@users.noreply.github.com>
2023-08-09 05:15:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+function App() {
+  return <>
+    <div id="wrapper">
+        <Sidebar/>
+        <Dashboard/>
+</div>
+  </>
+}
+
+export default App;
